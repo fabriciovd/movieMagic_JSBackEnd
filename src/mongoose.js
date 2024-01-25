045c1c22cc4689f347eea771cc99f11b7fbeaf5b
fabriciovd <YavorPetkov@students.softuni.bg>
2024-01-25 17:12:31 +0200

    1

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,42 +1,18 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const  {MongoClient} = require('mongodb');
-const { log } = require("handlebars");
+const express = require('express');
+const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://test:2bt.vsp.wVj7THV@atlascluster.szuql7f.mongodb.net/";
+const configHandlebars = require('./config/configHandlebars');
+const configExpress = require('./config/configExpress');
+const routes = require('./routes');
 
-// import { MongoClient } from 'mongodb';
+const app = express();
+const port = 5000;
+// const uri = "mongodb+srv://test:2bt.vsp.wVj7THV@atlascluster.szuql7f.mongodb.net/";
+const localURI = 'mongodb://localhost:27017'
 
-/*
- * Requires the MongoDB Node.js Driver
- * https://mongodb.github.io/node-mongodb-native
- */
 
-// const filter = {
-//   'username': 'fmiller'
-// };
-
-// const client = await MongoClient.connect(
-//   'mongodb+srv://test:2bt.vsp.wVj7THV@atlascluster.szuql7f.mongodb.net/'
-// );
-// const coll = client.db('sample_analytics').collection('customers');
-// const cursor = coll.find(filter);
-// const result = await cursor.toArray();
-// await client.close();
-
-const client  = new MongoClient(uri);
-
-client.connect().then(() => {
-    console.log("Connected successfully to server");
+mongoose.connect(localURI).then((res)=>{
+    console.log("Connected to DB");
+}).catch((err)=>{
+    console.log(err);
 })
-
-const coll = client.db('sample_analytics').collection('customers');
-
-const filter = {
-    'username': 'fmiller'
-  };
-
-coll.find(filter).toArray().then((result) => {
-    console.log(result);
-    client.close();
-});
